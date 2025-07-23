e99f14c886b0e22a6794ab9803967edba9d1b880
thinh ho <thinh.ho@gmail.com>
2025-07-23 13:01:23 -0400

    Add --profile option

--- keycloak-client/src/dist/keycloak-client.sh
+++ keycloak-client/src/dist/keycloak-client.sh
@@ -7,11 +7,14 @@ function help() {
   echo "  --debug-port <port>       Set debug port, default 9000"
   echo "  --debug-suspend           Suspend debug to wait for debugger to be attached"
   echo "  --socks5 <host> <port>    SOCKS5 proxy, default localhost / 8001"
+  echo "  --profile <profile>       Run the application in the specified profile configuration"
+
 }
 
 __debug_suspend="n"
 __debug_port=9000
 __socks5_options=""
+__profile=""
 
 while [ "$1" != "" ]; do
   case "$1" in
@@ -32,6 +35,11 @@ while [ "$1" != "" ]; do
     __socks5_options="-DsocksProxyHost=${__socks5_host} -DsocksProxyPort=${__socks5_port}"
     ;;
 
+  --profile)
+    shift
+    __profile="-Dspring.profiles.active=${1}"
+    ;;
+
   h|-h|--h|help|-help|--help)
     help
     exit 0
@@ -46,7 +54,7 @@ while [ "$1" != "" ]; do
   shift
 done
 
-export JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,address=${__debug_port},suspend=${__debug_suspend} ${__socks5_options}"
+export JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,address=${__debug_port},suspend=${__debug_suspend} ${__socks5_options} ${__profile}"
 export KEYCLOAK_CLIENT_OPTS=""
 
 ${__directory}/bin/keycloak-client
